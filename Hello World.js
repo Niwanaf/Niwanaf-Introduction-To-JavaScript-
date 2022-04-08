ae118eb176d336bd3a20c84d25573ce6dfe30e90
Niwanaf <95495758+Niwanaf@users.noreply.github.com>
2022-04-08 23:39:49 +0700

    menambahkan expression dan statement

--- Hello World.js
+++ Hello World.js
@@ -1,3 +1,15 @@
 console.log("Hello, World!");
 //console.log merupakan kode bawaan JavaScript untuk menampilkan pesan ke konsol
 //tanda("") menandakan bahwa pesan yang ingin ditampilkan merupakan sebuah string atau teks
+
+//Expression dan statement
+5;// Baris kode ini adalah expression karena interpreter akan membaca kode ini dan menampilkan angka 5
+2+3;// baris kode ini juga merupakan expression
+
+//statement
+var yourName;
+let yourAge;
+const numberOfDays; // declaration statement
+
+//Expression merupakan kode yang menghasilkan nilai
+//Statement menunjukan aksi yang dilakukan
