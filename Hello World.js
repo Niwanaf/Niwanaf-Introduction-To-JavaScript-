97ea0a83081359f418d7fb78256e0e008d858149
Niwanaf <95495758+Niwanaf@users.noreply.github.com>
2022-04-08 23:33:47 +0700

    membuat hello world

---
+++ Hello World.js
@@ -0,0 +1,3 @@
+console.log("Hello, World!");
+//console.log merupakan kode bawaan JavaScript untuk menampilkan pesan ke konsol
+//tanda("") menandakan bahwa pesan yang ingin ditampilkan merupakan sebuah string atau teks
